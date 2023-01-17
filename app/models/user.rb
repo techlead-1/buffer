aecc13f2fa733ee5656e3e33e84a0da3e4a6c0b7
Techlead <adesanya1ademola@gmail.com>
2023-01-17 22:42:41 +0100

    Added validation to database and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
+
+    validate :email, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_01_17_205349) do
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "username", null: false
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
