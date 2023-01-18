855a6d745bb50d25153502798c3cdd82a4be1651
Techlead <adesanya1ademola@gmail.com>
2023-01-18 19:03:14 +0100

    Added login feature

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
 #  registration
  get "sign_up", to: "registration#new"
  post "sign_up", to: "registration#create"
- delete "logout", to: "logout#destroy"
+ delete "logout", to: "session#destroy"
+
+#  Sign in
+get "sign_in", to: "session#new"
+post "sign_in", to: "session#create"
 end
